fae00fde7dcde1fffdb69645b84e33c79d0a8f30
Christopher Lam <127339027+clam365@users.noreply.github.com>
2025-06-16 18:38:58 -0400

    completed d3.js

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# ICE 03: The Other One
+# ICE 03: The Other One/Creative Coding
+Members: Just me!
 
-In this activity, you will complete a simplified version of A4, specifically using the option you did NOT chooose for that assignment.
+Contributions: For the creative coding, used D3.js to create 2D volcano contour lines. No Necessary things, the contour is just shown.
 
-### Instructions
+volcano.json originated from https://observablehq.com/@d3/volcano-contours/2
 
-1. Decide whether you want to work alone or with others in this ISP. If you are working with others, make sure to include a README in your final submission with all of your group members.
-   
-2. Go to the version of A4 you did NOT choose and complete the assignment with the following modifications:
-	* You should submit a pull request against THIS assignment, NOT A4.
-    * **Components:** You should start with your existing submission for A2 or A3.
-    * **Creative Coding:** You should choose which of the technologies on the list you want to explore. You must choose at least one but can do more than one.
-    * **As this is an in-class activity, we are NOT expecting the same level of complexity as A4,** although we are expecting you to demonstrate a basic understanding of the technologies in question.
-    * Your README should include your hosting link, the name of all group members, the contributions of each group member, and any instructions necessary to use the application. You can ignore the points in A4 about goals and challenges.
+base starter code used from https://d3-graph-gallery.com/graph/density2d_contour.html (edited to use json files and new format)
+
+Link:
 
--- app/page.js
+++ app/page.js
@@ -1,103 +1,62 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
+//Build Volcano Contours
+//volcano.json originated from https://observablehq.com/@d3/volcano-contours/2
+//base code used from https://d3-graph-gallery.com/graph/density2d_contour.html
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const svgRef = useRef();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    useEffect(() => {
+        //define width, height, and its margins
+        const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+        const width = 800;
+        const height = 600;
+
+        //create our svg and its bounds
+        const svg = d3.select(svgRef.current)
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        //in the initial starter code, it used a .csv, however the file I used is in .json and it is in a different format.
+        //this is the easier way, where we just grab the JSON values array and boom pass it through (no need to individually map)
+        d3.json("/volcano.json").then((contourValues) => {
+            const { width: gridWidth, height: gridHeight, values } = contourValues;
+
+            //create our contour lines
+            const contours = d3.contours()
+                .size([gridWidth, gridHeight])
+                .thresholds(20)(values);
+
+            //scale our SVG
+            const scaleX = width / gridWidth;
+            const scaleY = height / gridHeight;
+            const transform = d3.geoTransform({
+                point: function (x, y) {
+                    this.stream.point(x * scaleX, y * scaleY);
+                },
+            });
+
+            //we make it now!
+            const path = d3.geoPath().projection(transform);
+            svg.selectAll("path")
+                .data(contours)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("fill", "none")
+                .attr("stroke", "#69b3a2")
+                .attr("stroke-linejoin", "round");
+        });
+    }, []);
+
+    return (
+        <main className="flex items-center justify-center min-h-screen bg-white">
+            {/* Call our SVG to be drawn onto the HTML */}
+            <svg ref={svgRef}></svg>
+        </main>
+    );
 }
